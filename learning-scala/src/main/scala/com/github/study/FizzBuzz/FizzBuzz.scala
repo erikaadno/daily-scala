20d6fe19954c05a20eb397d8d48b286d63536171
erikaadno <erika.ando+github@bizreach.co.jp>
2019-10-08 12:08:19 +0900

    add FizzBuzz

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -4,7 +4,34 @@ package com.github.study.FizzBuzz
  * Created by tamaki on 2015/02/08.
  */
 object FizzBuzz {
-  def main(args: Array[String]): Unit = {
+
+  val list = (1 to 100)
+  // 一番スタンダードなやつ
+  def useMap = {
+    val result = list.map{ i =>
+      i match {
+        case i if i % 15 == 0 => "FizzBuzz"
+        case i if i % 5 == 0 => "Buzz"
+        case i if i % 3 == 0 => "Fizz"
+        case _ => i.toString
+      }
+    }
+    println(result.mkString("\n"))
+  }
+
+  def useFor = {
+    val result = for {
+      i <-list
+    } yield {
+      i match {
+        case i if i % 15 == 0 => "FizzBuzz"
+        case i if i % 5 == 0 => "Buzz"
+        case i if i % 3 == 0 => "Fizz"
+        case _ => i.toString
+      }
+    }
+
+    println(result.mkString("\n"))
 
   }
 }
